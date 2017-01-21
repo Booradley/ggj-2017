a8905fa6ffe46d4fe72a739dcfcc270450dcfee2
Bradley Adams <brad@bradleyadams.com>
2017-01-20 23:06:19 -0800

    DialogManager base

---
+++ GGJ2017/Assets/Scripts/Data/DialogData.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[CreateAssetMenu]
+public class DialogData : ScriptableObject
+{
+    [SerializeField]
+    private AudioClip _audioClip;
+
+    [SerializeField]
+    private bool _interrupt;
+}
