bb5e215f86169c28bb4b198f2164546863ece354
Warren <warrussell@gmail.com>
2017-01-21 00:33:30 -0800

    Initial dialog manager setup.

--- GGJ2017/Assets/Scripts/Data/DialogData.cs
+++ GGJ2017/Assets/Scripts/Data/DialogData.cs
@@ -9,4 +9,9 @@ public class DialogData : ScriptableObject
 
     [SerializeField]
     private bool _interrupt;
+
+	[SerializeField]
+	private float _delay;
+
+	public bool isInterupt { get { return _interrupt; } }
 }
