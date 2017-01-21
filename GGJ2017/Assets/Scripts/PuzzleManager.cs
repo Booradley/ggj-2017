5e8b67bcdca1748c13c8c5a652aa0d3f7a8514ce
Bradley Adams <brad@bradleyadams.com>
2017-01-20 23:58:35 -0800

    Initial PuzzleManager code

--- GGJ2017/Assets/Scripts/PuzzleManager.cs
+++ GGJ2017/Assets/Scripts/PuzzleManager.cs
@@ -1,15 +1,54 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System;
 
-public class PuzzleManager : MonoBehaviour {
+public class PuzzleManager : MonoBehaviour
+{
+    private static PuzzleManager _instance;
+    public static PuzzleManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = GameObject.FindObjectOfType<PuzzleManager>();
+            }
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+            return _instance;
+        }
+    }
+
+    [SerializeField]
+    private List<SegmentData> _segments;
+
+    private SegmentData _currentSegmentData;
+    private int _currentSegmentIndex = 0;
+    private Coroutine _segmentCoroutine;
+
+    private void Start()
+    {
+        PlayNextSegment();
+    }
+
+    private void PlayNextSegment()
+    {
+        if (_segmentCoroutine != null)
+        {
+            StopCoroutine(_segmentCoroutine);
+        }
+
+        if (_currentSegmentData != null)
+        {
+            _currentSegmentIndex++;
+        }
+
+        _currentSegmentData = _segments[_currentSegmentIndex];
+        _segmentCoroutine = StartCoroutine(RunSegmentSequence());
+    }
+
+    private IEnumerator RunSegmentSequence()
+    {
+        yield return null;
+    }
 }
