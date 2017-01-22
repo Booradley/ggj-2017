1851cb819b0dc537be639375462eff371a07c7a5
Bradley Adams <brad@bradleyadams.com>
2017-01-22 02:17:44 -0800

    Added hinge code

---
+++ GGJ2017/Assets/Scripts/HingedObjectTarget.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using Valve.VR.InteractionSystem;
+
+[RequireComponent(typeof(Interactable))]
+public class HingedObjectTarget : MonoBehaviour
+{
+    private Action<Hand> _onDetached;
+    private Vector3 _initialHingeTargetPosition;
+
+    public void Setup(Action<Hand> onDetached)
+    {
+        _onDetached = onDetached;
+        _initialHingeTargetPosition = transform.position;
+    }
+
+    private void OnDetachedFromHand(Hand hand)
+    {
+        transform.position = _initialHingeTargetPosition;
+
+        if (_onDetached != null)
+        {
+            _onDetached(hand);
+        }
+    }
+
+    public void HandAttachedUpdate(Hand hand)
+    {
+        if (hand.GetStandardInteractionButtonUp() || ((hand.controller != null) && hand.controller.GetPressUp(Valve.VR.EVRButtonId.k_EButton_Grip)))
+        {
+            hand.DetachObject(gameObject);
+        }
+    }
+}
