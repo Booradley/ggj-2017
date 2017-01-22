1a28a18c2d4486c5e879a93803b68628866eb37b
Warren <warrussell@gmail.com>
2017-01-22 03:32:18 -0800

    All assets put into prefabs. Breakable script for cup, vase, and pigybank

---
+++ GGJ2017/Assets/Scripts/Objects/Breakable.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using Valve.VR.InteractionSystem;
+
+public class Breakable : MonoBehaviour 
+{
+	[SerializeField]
+	protected Rigidbody _mainRigidBody = null;
+
+	[SerializeField]
+	protected Throwable _mainThrowable = null;
+
+	[SerializeField]
+	protected Collider _mainCollider = null;
+
+	[SerializeField]
+	protected GameObject _mainMesh = null;
+
+	[SerializeField]
+	protected GameObject _brokenMesh = null;
+
+	[SerializeField]
+	protected bool _canBreakOnAwake = false;
+
+	protected bool _canBreak;
+
+	protected void Awake()
+	{
+		_brokenMesh.SetActive(false);
+
+		SetCanBreak(_canBreakOnAwake);
+	}
+
+	public void SetCanBreak(bool value)
+	{
+		_canBreak = value;
+	}
+
+	public virtual void OnPickedUp()
+	{
+	}
+
+	protected void OnCollisionEnter(Collision collision)
+	{
+		if (!_canBreak)
+			return;
+
+		if (collision.relativeVelocity.magnitude > 5f)
+		{
+			_mainMesh.SetActive(false);
+			_mainCollider.enabled = false;
+			_mainRigidBody.isKinematic = true;
+
+			_brokenMesh.SetActive(true);
+
+			Rigidbody[] rigidBodies = _brokenMesh.GetComponentsInChildren<Rigidbody>();
+			foreach (Rigidbody rb in rigidBodies)
+			{
+				rb.AddExplosionForce(collision.relativeVelocity.magnitude * 2f, collision.contacts[0].point, 0.5f);
+			}
+
+			_canBreak = false;
+
+			OnBroken();
+		}
+	}
+
+	protected virtual void OnBroken()
+	{
+	}
+}
--- GGJ2017/Assets/Scripts/Objects/FlashLight.cs
+++ GGJ2017/Assets/Scripts/Objects/FlashLight.cs
@@ -5,23 +5,9 @@ using Valve.VR.InteractionSystem;
 public class FlashLight : MonoBehaviour 
 {
 	[SerializeField]
-	private Light _light = null;
-
-	[SerializeField]
-	private MeshRenderer _lightbulbMeshRenderer = null;
-
-	[SerializeField]
-	private Material _lightOffMat = null;
-
-	[SerializeField]
-	private Material _lightOnMat = null;
+	private ToggleLight _toggleLight = null;
 
 	private Hand _hand = null;
-
-	private void Start()
-	{
-		SetLight(false);
-	}
 
 	public void OnAttachedToHand(Hand hand)
 	{
@@ -33,34 +19,20 @@ public class FlashLight : MonoBehaviour
 		_hand = null;
 	}
 
-	public void SetLight(bool turnOn)
-	{
-		_light.enabled = turnOn;
-
-		if (turnOn)
-		{
-			_lightbulbMeshRenderer.material = _lightOnMat;
-		}
-		else
-		{
-			_lightbulbMeshRenderer.material = _lightOffMat;
-		}
-	}
-
 	private void Update()
 	{
 		if (_hand != null && _hand.controller != null)
 		{
 			if (_hand.controller.GetPressDown(Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad))
 			{
-				SetLight(!_light.enabled);
+				_toggleLight.SetLight(!_toggleLight.isOn);
 			}
 		}
 
 		// For testing
 		if (Input.GetKeyDown(KeyCode.L))
 		{
-			SetLight(!_light.enabled);
+			_toggleLight.SetLight(!_toggleLight.isOn);
 		}
 	}
 }
